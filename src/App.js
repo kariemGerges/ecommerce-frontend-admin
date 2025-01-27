a3f12dd96f2ad80a7cd419629a44ca217327a4fa
kariem <kiko1086@gmail.com>
2025-01-27 04:29:45 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+    return (
+        <div className="text-center mt-5 text-danger fs-1">
+         admin
+        </div>      
+            
+        
+    );
+}
+
+export default App;
